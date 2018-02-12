2a3d9e8e3480ccd47f751fb60cb66c624090b4e4
manojmechon <35764855+manojmechon@users.noreply.github.com>
2018-02-12 13:26:14 +0530

    line.c

---
+++ line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+char a[40];
+int i,c=0;
+printf("Enter the paragraph");
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]=='.')
+{
+c++;
+}
+}
+printf("%d",c);
+}
