ec3a0476eff1c08ae2da3dedb00067be0837c335
manojmechon <35764855+manojmechon@users.noreply.github.com>
2018-02-12 13:45:02 +0530

    gilly.c

---
+++ gilly.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int f1=0,f2=1,n,i,c;
+printf("enter a number");
+scanf("%d",&n);
+printf("%d%d",f1,f2);
+for(i=1;i<n;i++)
+{
+c=f1+f2;
+f1=f2;
+f2=c;
+printf("%d",c);
+}
+}
