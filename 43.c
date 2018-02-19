f77a3b87fdfab1cc8df09687b75ccb0acac6e64e
manojmechon <35764855+manojmechon@users.noreply.github.com>
2018-02-19 13:49:21 +0530

    43.c

---
+++ 43.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s2[50],char s3[50];
+clrscr();
+printf("enter string2");
+scanf("%s",s2);
+printf("enter string3");
+scanf("%s",s3);
+printf("%s%s",s2,s3);
+getch();
+}
