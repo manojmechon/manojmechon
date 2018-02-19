6a31132d9f1efe6bf581c4a48fa3e1136b9aa045
manojmechon <35764855+manojmechon@users.noreply.github.com>
2018-02-19 13:55:26 +0530

    44.c

---
+++ 44.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i;
+printf("enter the number:");
+scanf("%d",&n);
+if(n<10)
+{
+	printf("yes");
+}
+else
+{
+	printf("no");
+}
+return 0;
+}
